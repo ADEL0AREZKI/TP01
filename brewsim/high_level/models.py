913df5cdfb69055d6fe26a926903f433190942f5
Adel Arezki <rzd3134a@u1-105-01d.univ-tlse3.fr>
2023-10-03 11:31:21 +0200

    test.py

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -1,85 +1,105 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    nom = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nom
 
 
 class Departement(models.Model):
     numero = models.IntegerField()
     prixm2 = models.IntegerField()
 
-
-class Ingredient(models.Model):
-    nom = models.CharField(max_length=100)
+    def __str__(self):
+        return f"Département {self.numero}"
 
 
 class Prix(models.Model):
-    ingredient = models.ForeignKey(
-        Ingredient,  # ou "self",
-        on_delete=models.PROTECT,
-        # blank=True, null=True,
-        # related_name="+",
-    )
-    departement = models.ForeignKey(
-        Departement,  # ou "self",
-        on_delete=models.PROTECT,
-        # blank=True, null=True,
-        # related_name="+",
-    )
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
     prix = models.IntegerField()
 
-
-class Machine(models.Model):
-    nom = models.CharField(max_length=100)
-    prix = models.IntegerField()
+    def __str__(self):
+        return (
+            f"Ingrédient {self.ingredient} du département"
+            f" {self.departement.numero} à {self.prix}€"
+        )
 
 
 class QuantiteIngredient(models.Model):
-    ingredient = models.ForeignKey(
-        Ingredient,  # ou "self",
-        on_delete=models.PROTECT,
-        # blank=True, null=True,
-        # related_name="+",
-    )
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
     quantite = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.ingredient.nom} quantité {self.quantite}"
+
+    def couts(self, departement):
+        return (
+            self.ingredient.prix_set.get(departement__numero=departement).prix
+            * self.quantite
+        )
+
+
+class Machine(models.Model):
+    nom = models.CharField(max_length=50)
+    prix = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nom}"
 
 
 class Action(models.Model):
-    machine = models.ForeignKey(
-        Machine,  # ou "self",
-        on_delete=models.PROTECT,
-        # blank=True, null=True,
-        # related_name="+",
-    )
-    commande = models.CharField(max_length=100)
+    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    commande = models.CharField(max_length=50)
     duree = models.IntegerField()
-    ingredients = models.ManyToManyField(QuantiteIngredient)
     action = models.ForeignKey(
-        "self",  # ou "self",
+        "self",
         on_delete=models.PROTECT,
         blank=True,
         null=True,
         related_name="+",
     )
 
+    def __str__(self):
+        return f"{self.commande}"
+
 
 class Recette(models.Model):
-    nom = models.CharField(max_length=100)
-    action = models.ForeignKey(
-        Action,  # ou "self",
-        on_delete=models.PROTECT,
-        # blank=True, null=True,
-        # related_name="+",
-    )
+    nom = models.CharField(max_length=50)
+    action = models.ForeignKey(Action, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.nom}"
 
 
 class Usine(models.Model):
-    departement = models.ForeignKey(
-        Departement,  # ou "self",
-        on_delete=models.PROTECT,
-        # blank=True, null=True,
-        # related_name="+",
-    )
+    departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
     taille = models.IntegerField()
     machines = models.ManyToManyField(Machine)
     recettes = models.ManyToManyField(Recette)
     stocks = models.ManyToManyField(QuantiteIngredient)
+
+    def __str__(self):
+        return f"Usine du {self.departement}"
+
+    def somme_machines(self):
+        som = 0
+        for i in self.machines.all():
+            som = som + i.prix
+        return som
+
+    def somme_stocks(self):
+        som = 0
+        for i in self.stocks.all():
+            som = som + i.couts(self.departement.numero)
+        return som
+
+    def costs(self):
+        return (
+            self.departement.prixm2 * self.taille
+            + self.somme_machines()
+            + self.somme_stocks()
+        )
--- brewsim/high_level/tests.py
+++ brewsim/high_level/tests.py
@@ -1,1 +1,38 @@
 # Create your tests here.
+from django.test import TestCase
+
+from .models import Departement, Ingredient, Machine, Prix, QuantiteIngredient, Usine
+
+
+class MachineModelTests(TestCase):
+    def test_usine_creation(self):
+        depart = Departement.objects.create(numero=31, prixm2=2000)
+        houblon = Ingredient.objects.create(nom="houblon")
+        orge = Ingredient.objects.create(nom="orge")
+        four = Machine.objects.create(nom="four", prix=1000)
+        malaxeur = Machine.objects.create(nom="malaxeur", prix=2000)
+
+        Prix.objects.create(ingredient=houblon, departement=depart, prix=20)
+        Prix.objects.create(ingredient=orge, departement=depart, prix=10)
+
+        stock_houblon = QuantiteIngredient.objects.create(
+            ingredient=houblon, quantite=50
+        )
+        stock_orge = QuantiteIngredient.objects.create(ingredient=orge, quantite=100)
+
+        usine = Usine.objects.create(departement=depart, taille=50)
+        usine.machines.add(four)
+        usine.machines.add(malaxeur)
+        usine.stocks.add(stock_houblon)
+        usine.stocks.add(stock_orge)
+
+        self.assertEqual(Usine.objects.first().costs(), 105000)
+
+
+"""
+class MachineModelTests(TestCase):
+	def test_usine_creation(self):
+		self.assertEqual(Machine.objects.count(), 0)
+		Machine.objects.create(nom="four",prix=1_000)
+		self.assertEqual(Machine.objects.count(), 1)
+"""
